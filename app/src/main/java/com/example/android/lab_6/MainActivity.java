e82c0c7b0db2da8c4d5e88fcf29f4066752a6341
dsullivan21 <deveshsullivan21@gmail.com>
2020-03-09 12:40:57 -0500

    module1

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.android.lab_6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private final LatLng mDestinationLatlng = new LatLng(43.0753, -89.4042306);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions()
+            .position(mDestinationLatlng)
+            .title("destination"));
+
+        });
+
     }
 }
